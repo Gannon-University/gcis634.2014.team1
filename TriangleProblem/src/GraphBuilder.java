b517dc0fe6a7eef6505e1e1124807f73bbc918a6
Hassan Al Rawi <alrawi001@knights.gannon.edu>
2014-10-20 21:27:15 -0400

    Added ability to move nodes

--- TriangleProblem/src/GraphBuilder.java
+++ TriangleProblem/src/GraphBuilder.java
@@ -7,7 +7,6 @@ import edu.uci.ics.jung.algorithms.layout.CircleLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.graph.SparseMultigraph;
 import edu.uci.ics.jung.graph.util.EdgeType;
-import edu.uci.ics.jung.visualization.BasicVisualizationServer;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse;
 import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
@@ -17,26 +16,26 @@ import edu.uci.ics.jung.visualization.renderers.Renderer.VertexLabel.Position;
 public class GraphBuilder{
 
 	private static SparseMultigraph<Integer, String> graph;
-	
+
 	public static void buildGraph(String[] fileContent) {
 		graph = new SparseMultigraph<Integer, String>();
 		int lastindex = 0;
-		
+
 		for (int i = 0; i < fileContent.length; i++) {
 			if (fileContent[i] == null) {
 				continue;
 			}
-			
+
 			String command = CommandsAndValuesConverter.getCommand(fileContent[i]);
 			String value = CommandsAndValuesConverter.getValue(fileContent[i]);
-			
+
 			graph.addVertex(i);
 			if (i != 0) {
 				graph.addEdge(i + " " + command, lastindex, i,
 						EdgeType.DIRECTED);
 			}
-			
-			
+
+
 			if (command.equalsIgnoreCase("if_icmplt")
 					|| command.equalsIgnoreCase("if_icmpgt")
 					|| command.equalsIgnoreCase("if_icmple")
@@ -47,93 +46,93 @@ public class GraphBuilder{
 						i, EdgeType.DIRECTED);
 
 			}
-			
+
 			lastindex = i;
 		}
-		
+
 		graphViewer();
-		
+
 	}
-	
+
 	public static void buildGraphForAPath(ArrayList<String> fileContent) {
 		graph = new SparseMultigraph<Integer, String>();
 		int lastindex = 0;
-		
+
 		for (int i = 0; i < fileContent.size(); i++) {
 			if (fileContent.get(i) == null) {
 				continue;
 			}
-			
+
 			graph.addVertex(Integer.parseInt(fileContent.get(i)));
 			if (i != 0) {
 				try {
-				graph.addEdge(fileContent.get(lastindex) + " to " + fileContent.get(i), Integer.parseInt(fileContent.get(lastindex)), Integer.parseInt(fileContent.get(i)),
-						EdgeType.DIRECTED);
-			
+					graph.addEdge(fileContent.get(lastindex) + " to " + fileContent.get(i), Integer.parseInt(fileContent.get(lastindex)), Integer.parseInt(fileContent.get(i)),
+							EdgeType.DIRECTED);
+
 				} finally {
-					System.out.println("Edge exists");
+					//					System.out.println("Edge exists");
 				}
 			}
 			lastindex = i;
 		}
-		
+
 		graphViewer();
-		
+
 	}
-	
-	
-//	public static void buildGraphForEverypath(ArrayList<ArrayList<String>> fileContent) {
-//		for (ArrayList<String> path : fileContent) {
-//			graph = new SparseMultigraph<Integer, String>();
-//			int lastindex = 0;
-//			for (int i = 0; i < path.size(); i++) {
-//				if (path.get(i) == null) {
-//					continue;
-//				}
-//
-//				graph.addVertex(Integer.parseInt(path.get(i)));
-//				if (i != 0) {
-//					graph.addEdge(fileContent.get(lastindex) + " to "
-//							+ fileContent.get(i),
-//							Integer.parseInt(path.get(lastindex)),
-//							Integer.parseInt(path.get(i)), EdgeType.DIRECTED);
-//				}
-//
-//				lastindex = i;
-//			}
-//
-//			graphViewer();
-//		}
-//	}
-	
+
+
+	//	public static void buildGraphForEverypath(ArrayList<ArrayList<String>> fileContent) {
+	//		for (ArrayList<String> path : fileContent) {
+	//			graph = new SparseMultigraph<Integer, String>();
+	//			int lastindex = 0;
+	//			for (int i = 0; i < path.size(); i++) {
+	//				if (path.get(i) == null) {
+	//					continue;
+	//				}
+	//
+	//				graph.addVertex(Integer.parseInt(path.get(i)));
+	//				if (i != 0) {
+	//					graph.addEdge(fileContent.get(lastindex) + " to "
+	//							+ fileContent.get(i),
+	//							Integer.parseInt(path.get(lastindex)),
+	//							Integer.parseInt(path.get(i)), EdgeType.DIRECTED);
+	//				}
+	//
+	//				lastindex = i;
+	//			}
+	//
+	//			graphViewer();
+	//		}
+	//	}
+
 	public static void graphViewer() {
 		// The Layout<V, E> is parameterized by the vertex and edge types
-				Layout<Integer, String> layout = new CircleLayout<Integer, String>(
-						graph);
-//				Layout layout = new FRLayout2(myGraph);
-				layout.setSize(new Dimension(800, 800)); // sets the initial size of the
-															// layout space
-				// The BasicVisualizationServer<V,E> is parameterized by the vertex and
-				// edge types
-				VisualizationViewer<Integer, String> vv = new VisualizationViewer<Integer, String>(
-						layout);
-				vv.setPreferredSize(new Dimension(900, 900)); // Sets the viewing area
-																// size
-				vv.getRenderContext().setVertexLabelTransformer(
-						new ToStringLabeller<Integer>());
-				vv.getRenderContext().setEdgeLabelTransformer(
-						new ToStringLabeller<String>());
-				vv.getRenderer().getVertexLabelRenderer().setPosition(Position.CNTR);
-				
-				DefaultModalGraphMouse m_graphmouse = new DefaultModalGraphMouse();
-				m_graphmouse.setMode(ModalGraphMouse.Mode.PICKING);
+		Layout<Integer, String> layout = new CircleLayout<Integer, String>(
+				graph);
+		//				Layout layout = new FRLayout2(myGraph);
+		layout.setSize(new Dimension(800, 800)); // sets the initial size of the
+		// layout space
+		// The BasicVisualizationServer<V,E> is parameterized by the vertex and
+		// edge types
+		VisualizationViewer<Integer, String> vv = new VisualizationViewer<Integer, String>(
+				layout);
+		vv.setPreferredSize(new Dimension(900, 900)); // Sets the viewing area
+		// size
+		vv.getRenderContext().setVertexLabelTransformer(
+				new ToStringLabeller<Integer>());
+		vv.getRenderContext().setEdgeLabelTransformer(
+				new ToStringLabeller<String>());
+		vv.getRenderer().getVertexLabelRenderer().setPosition(Position.CNTR);
 
-				vv.setGraphMouse(m_graphmouse);
-				JFrame frame = new JFrame("Simple Graph View");
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.getContentPane().add(vv);
-				frame.pack();
-				frame.setVisible(true);
+		DefaultModalGraphMouse m_graphmouse = new DefaultModalGraphMouse();
+		m_graphmouse.setMode(ModalGraphMouse.Mode.PICKING);
+
+		vv.setGraphMouse(m_graphmouse);
+		JFrame frame = new JFrame("Simple Graph View");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(vv);
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 }
